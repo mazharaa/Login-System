5eec28140a95caa3a21d2a42fd671b9ed7b4b0d5
mazharaa <mazharaa32@gmail.com>
2023-02-06 00:53:37 +0700

    Update getGrantedAuthorities

--- src/main/java/com/azhar/LoginSystem/config/ProjectUsernamePwdAuthenticationProvider.java
+++ src/main/java/com/azhar/LoginSystem/config/ProjectUsernamePwdAuthenticationProvider.java
@@ -1,7 +1,10 @@
 package com.azhar.LoginSystem.config;
 
 import com.azhar.LoginSystem.model.Customer;
+import com.azhar.LoginSystem.model.Privilege;
+import com.azhar.LoginSystem.model.User;
 import com.azhar.LoginSystem.repository.CustomerRepository;
+import com.azhar.LoginSystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,12 +17,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class ProjectUsernamePwdAuthenticationProvider implements AuthenticationProvider {
     @Autowired
-    private CustomerRepository customerRepository;
+    private UserRepository userRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -28,18 +33,26 @@ public class ProjectUsernamePwdAuthenticationProvider implements AuthenticationP
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
         String pwd = authentication.getCredentials().toString();
-        List<Customer> customers = customerRepository.findByEmail(username);
-        if (customers.size() > 0) {
-            if (passwordEncoder.matches(pwd, customers.get(0).getPwd())) {
-                List<GrantedAuthority> authorities = new ArrayList<>();
-                authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
-                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+        List<User> user = userRepository.findByUsername(username);
+        if (user.size() > 0) {
+            if (passwordEncoder.matches(pwd, user.get(0).getPassword())) {
+                return new UsernamePasswordAuthenticationToken(username, pwd, getGrantedAuthorities(user.get(0).getRole().getPrivileges()));
             } else {
                 throw new BadCredentialsException("Invalid Password!");
             }
         } else {
             throw new BadCredentialsException("User doesn't exist!");
         }
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(Collection<Privilege> privileges) {
+        List<GrantedAuthority> grantedAuthorities =new ArrayList<>();
+
+        for (Privilege privilege : privileges) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(privilege.getName()));
+        }
+
+        return grantedAuthorities;
     }
 
     @Override
